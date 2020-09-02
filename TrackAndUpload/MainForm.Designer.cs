0a8deae03fe3c8eec53eabe5ed86a9d236cf6a57
Deividas Šeimys <deividas.seimys@gmail.com>
2020-09-02 19:29:54 +0300

    Created tracking

--- TrackAndUpload/MainForm.Designer.cs
+++ TrackAndUpload/MainForm.Designer.cs
@@ -29,12 +29,149 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnSelectPath = new System.Windows.Forms.Button();
+            this.lTrackedPath = new System.Windows.Forms.Label();
+            this.eTrackedPath = new System.Windows.Forms.TextBox();
+            this.lChangesLog = new System.Windows.Forms.Label();
+            this.eChangesLog = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.lTracking = new System.Windows.Forms.Label();
+            this.eTracking = new System.Windows.Forms.TextBox();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // btnSelectPath
+            // 
+            this.btnSelectPath.Location = new System.Drawing.Point(12, 403);
+            this.btnSelectPath.Name = "btnSelectPath";
+            this.btnSelectPath.Size = new System.Drawing.Size(128, 35);
+            this.btnSelectPath.TabIndex = 0;
+            this.btnSelectPath.Text = "Select folder";
+            this.btnSelectPath.UseVisualStyleBackColor = true;
+            this.btnSelectPath.Click += new System.EventHandler(this.btnSelectPath_Click);
+            // 
+            // lTrackedPath
+            // 
+            this.lTrackedPath.AutoSize = true;
+            this.lTrackedPath.Location = new System.Drawing.Point(12, 9);
+            this.lTrackedPath.Name = "lTrackedPath";
+            this.lTrackedPath.Size = new System.Drawing.Size(135, 15);
+            this.lTrackedPath.TabIndex = 1;
+            this.lTrackedPath.Text = "Currently tracked folder:";
+            // 
+            // eTrackedPath
+            // 
+            this.eTrackedPath.Enabled = false;
+            this.eTrackedPath.Location = new System.Drawing.Point(146, 6);
+            this.eTrackedPath.Name = "eTrackedPath";
+            this.eTrackedPath.ReadOnly = true;
+            this.eTrackedPath.Size = new System.Drawing.Size(642, 23);
+            this.eTrackedPath.TabIndex = 2;
+            // 
+            // lChangesLog
+            // 
+            this.lChangesLog.AutoSize = true;
+            this.lChangesLog.Location = new System.Drawing.Point(64, 43);
+            this.lChangesLog.Name = "lChangesLog";
+            this.lChangesLog.Size = new System.Drawing.Size(76, 15);
+            this.lChangesLog.TabIndex = 1;
+            this.lChangesLog.Text = "Changes log:";
+            // 
+            // eChangesLog
+            // 
+            this.eChangesLog.Enabled = false;
+            this.eChangesLog.Location = new System.Drawing.Point(146, 40);
+            this.eChangesLog.Multiline = true;
+            this.eChangesLog.Name = "eChangesLog";
+            this.eChangesLog.ReadOnly = true;
+            this.eChangesLog.Size = new System.Drawing.Size(642, 302);
+            this.eChangesLog.TabIndex = 2;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(526, 403);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(128, 35);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start tracking";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(660, 403);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(128, 35);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop tracking";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // lTracking
+            // 
+            this.lTracking.AutoSize = true;
+            this.lTracking.Location = new System.Drawing.Point(526, 351);
+            this.lTracking.Name = "lTracking";
+            this.lTracking.Size = new System.Drawing.Size(76, 15);
+            this.lTracking.TabIndex = 1;
+            this.lTracking.Text = "Currently on:";
+            // 
+            // eTracking
+            // 
+            this.eTracking.BackColor = System.Drawing.Color.Red;
+            this.eTracking.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.eTracking.Enabled = false;
+            this.eTracking.Location = new System.Drawing.Point(608, 348);
+            this.eTracking.Name = "eTracking";
+            this.eTracking.ReadOnly = true;
+            this.eTracking.Size = new System.Drawing.Size(180, 23);
+            this.eTracking.TabIndex = 2;
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Track and Update";
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.eTracking);
+            this.Controls.Add(this.lTracking);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.eChangesLog);
+            this.Controls.Add(this.lChangesLog);
+            this.Controls.Add(this.eTrackedPath);
+            this.Controls.Add(this.lTrackedPath);
+            this.Controls.Add(this.btnSelectPath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Track and Upload";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSelectPath;
+        private System.Windows.Forms.Label lTrackedPath;
+        private System.Windows.Forms.TextBox eTrackedPath;
+        private System.Windows.Forms.Label lChangesLog;
+        private System.Windows.Forms.TextBox eChangesLog;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label lTracking;
+        private System.Windows.Forms.TextBox eTracking;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
     }
 }
 
--- TrackAndUpload/MainForm.cs
+++ TrackAndUpload/MainForm.cs
@@ -1,21 +1,168 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace TrackAndUpload
 {
     public partial class MainForm : Form
     {
+        private bool _exit;
+        private bool _running;
+
         public MainForm()
         {
             InitializeComponent();
+            UpdateControls();
         }
 
+        private void btnSelectPath_Click(object sender, EventArgs e)
+        {
+            using (var f = new FolderBrowserDialog())
+            {
+                if (f.ShowDialog() != DialogResult.OK) 
+                    return;
+                
+                eTrackedPath.Text = f.SelectedPath;
+            }
+            UpdateControls();
+        }
+
+        private void TrackChanges()
+        {
+            try
+            {
+                ClearLog();
+                _exit = false;
+                _running = true;
+                UpdateControls();
+
+                using (var watcher = new FileSystemWatcher())
+                {
+                    watcher.Path = eTrackedPath.Text;
+
+                    // Watch for changes in LastAccess and LastWrite times, and
+                    // the renaming of files or directories.
+                    watcher.NotifyFilter = NotifyFilters.LastAccess
+                                           | NotifyFilters.LastWrite
+                                           | NotifyFilters.FileName
+                                           | NotifyFilters.DirectoryName;
+
+                    // Only watch text files.
+                    watcher.Filter = "*.txt";
+
+                    // Add event handlers.
+                    watcher.Changed += OnChanged;
+                    watcher.Created += OnChanged;
+                    watcher.Deleted += OnChanged;
+                    watcher.Renamed += OnRenamed;
+
+                    // Begin watching.
+                    watcher.EnableRaisingEvents = true;
+
+                    // Wait for the user to quit the program.
+                    while (!_exit)
+                    {
+                    }
+                }
+            }
+            finally
+            {
+                _running = false;
+                UpdateControls();
+            }
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            UpdateLog($"File: {e.OldFullPath} renamed to {e.FullPath}");
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            UpdateLog($"File: {e.FullPath} {e.ChangeType}");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e) => _exit = true;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var thread = new Thread(TrackChanges)
+            {
+                IsBackground = true
+            };
+            thread.Start();
+        }
+
+        private delegate void UpdateControlsDelegate();
+
+        private void UpdateControls()
+        {
+            if (Disposing) return;
+
+            if (InvokeRequired)
+            {
+                var d = new UpdateControlsDelegate(UpdateControls);
+                Invoke(d);
+            }
+            else
+            {
+                eTracking.BackColor = _running ? Color.Green : Color.Red;
+                btnStop.Enabled = !string.IsNullOrEmpty(eTrackedPath.Text) && _running;
+                btnStart.Enabled = !string.IsNullOrEmpty(eTrackedPath.Text) && !_running;
+                btnSelectPath.Enabled = !_running;
+            }
+        }
+
+        private delegate void UpdateLogDelegate(string line);
+
+        private void UpdateLog(string line)
+        {
+            if (Disposing) return;
+
+            if (InvokeRequired)
+            {
+                var d = new UpdateLogDelegate(UpdateLog);
+                Invoke(d, line);
+            }
+            else
+            {
+                eChangesLog.Text += line;
+                eChangesLog.Text += Environment.NewLine;
+            }
+        }
+
+        private delegate void ClearLogDelegate();
+
+        private void ClearLog()
+        {
+            if (Disposing) return;
+
+            if (InvokeRequired)
+            {
+                var d = new ClearLogDelegate(ClearLog);
+                Invoke(d);
+            }
+            else
+            {
+                eChangesLog.Text = string.Empty;
+            }
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (WindowState != FormWindowState.Minimized) return;
+
+            Hide();
+            notifyIcon.Visible = true;
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
     }
 }
